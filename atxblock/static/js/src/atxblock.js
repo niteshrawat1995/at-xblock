0b691c40d4a992b17da3356f29b1ac2ba666e149
Suprgya Bhushan <suprgya.56@iiitb.org>
2018-12-04 18:52:29 +0530

    n

--- atxblock/static/js/src/atxblock.js
+++ atxblock/static/js/src/atxblock.js
@@ -7,14 +7,14 @@ function onPageLoad() {
   jf.type='text/javascript';
   jf.id='ToolBar';
   d.getElementsByTagName('head')[0].appendChild(jf);
-  //d.getElementById('bcsession').style.backgroundColor = window.localStorage.getItem('bColour');
-  //var c = ["a","abbr","acronym","address","applet","area","article","aside","audio","b","base","basefont","bdi","bdo","big","blockquote","body","br","button","canvas","caption","center","cite","code","col","colgroup","command","datalist","dd","del","details","dfn","dir","div","dl","dt","em","embed","fieldset","figcaption","figure","font","footer","form","frame","frameset","h1","h2","h3","h4","h5","h6","head","header","hgroup","hr","html","i","iframe","img","input","ins","kbd","keygen","label","legend","li","link","map","mark","menu","meta","meter","nav","noframes","noscript","object","ol","optgroup","option","output","p","param","pre","progress","q","rp","rt","ruby","s","samp","script","section","select","small","source","strike","strong","style","sub","summary","sup","table","tbody","td","textarea","tfoot","th","thead","time","title","tr","track","tt","u","ul","var","video","wbr"];
+  d.getElementById('bcsession').style.backgroundColor = window.localStorage.getItem('bColour');
+  var c = ["a","abbr","acronym","address","applet","area","article","aside","audio","b","base","basefont","bdi","bdo","big","blockquote","body","br","button","canvas","caption","center","cite","code","col","colgroup","command","datalist","dd","del","details","dfn","dir","div","dl","dt","em","embed","fieldset","figcaption","figure","font","footer","form","frame","frameset","h1","h2","h3","h4","h5","h6","head","header","hgroup","hr","html","i","iframe","img","input","ins","kbd","keygen","label","legend","li","link","map","mark","menu","meta","meter","nav","noframes","noscript","object","ol","optgroup","option","output","p","param","pre","progress","q","rp","rt","ruby","s","samp","script","section","select","small","source","strike","strong","style","sub","summary","sup","table","tbody","td","textarea","tfoot","th","thead","time","title","tr","track","tt","u","ul","var","video","wbr"];
   //var all = d.getElementsByTagName("*");
   //var alinks = d.getElementsByTagName("a");
 
 
 
-  /*for (var a = 0; a < c.length; a++){
+  for (var a = 0; a < c.length; a++){
        $(c[a]).each(function(c, d){
          var e = $(d).css("font-size");
          e = parseFloat(e);
@@ -35,6 +35,6 @@ function onPageLoad() {
   for (var b = 0, max=c.length; b < max ; b++){
       $(c[b]).css("color", window.localStorage.getItem("textColour"));
   };
-  $("a").css("color", window.localStorage.getItem("linkColour"));*/
+  $("a").css("color", window.localStorage.getItem("linkColour"));
 
 }
