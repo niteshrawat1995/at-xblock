493800370084bb89244c0ff8f00010feaf0e3f98
Suprgya Bhushan <suprgya.56@iiitb.org>
2018-12-05 09:38:21 +0530

    n

--- atxblock/static/js/src/atxblock.js
+++ atxblock/static/js/src/atxblock.js
@@ -8,7 +8,7 @@ function onPageLoad() {
   jf.id='ToolBar';
   d.getElementsByTagName('head')[0].appendChild(jf);
   d.getElementById('bcsession').style.backgroundColor = window.localStorage.getItem('bColour');
-  
+
 
   var c = ["a","abbr","acronym","address","applet","area","article","aside","audio","b","base","basefont","bdi","bdo","big","blockquote","body","br","button","canvas","caption","center","cite","code","col","colgroup","command","datalist","dd","del","details","dfn","dir","div","dl","dt","em","embed","fieldset","figcaption","figure","font","footer","form","frame","frameset","h1","h2","h3","h4","h5","h6","head","header","hgroup","hr","html","i","iframe","img","input","ins","kbd","keygen","label","legend","li","link","map","mark","menu","meta","meter","nav","noframes","noscript","object","ol","optgroup","option","output","p","param","pre","progress","q","rp","rt","ruby","s","samp","script","section","select","small","source","strike","strong","style","sub","summary","sup","table","tbody","td","textarea","tfoot","th","thead","time","title","tr","track","tt","u","ul","var","video","wbr"];
   //var all = d.getElementsByTagName("*");
@@ -38,5 +38,7 @@ function onPageLoad() {
       $(c[b]).css("color", window.localStorage.getItem("textColour"));
   };
   $("a").css("color", window.localStorage.getItem("linkColour"));
+
+  d.querySelector('.sequence-nav-button.button-nex').style.color = window.localStorage.getItem("linkColour") ;
 
 }
